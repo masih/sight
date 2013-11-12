0adb1d84e40bc96dcffc06f01e3cb1c4b8e24aff
masih <masiih@gmail.com>
2013-11-12 21:28:48 +0000

    fixed the file header

--- src/main/java/org/mashti/sight/ChartExportUtils.java
+++ src/main/java/org/mashti/sight/ChartExportUtils.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of sight.
+ *
+ * sight is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sight.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.mashti.sight;
 
 import java.awt.Rectangle;
