0360fa425122a0b96ad20a5048bef939a9550564
masih <masiih@gmail.com>
2015-05-28 08:34:03 +0100

    refactoring

--- src/main/java/org/derkani/sight/ChartExportUtils.java
+++ src/main/java/org/derkani/sight/ChartExportUtils.java
@@ -24,39 +24,56 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.mashti.sight;
+package org.derkani.sight;
 
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.commons.io.IOUtils;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.Plot;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.xy.XYDataset;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
 
 /**
  * @author Masih Hajiarabderkani (mh638@st-andrews.ac.uk)
  */
-public class ChartMergeUtils {
+public final class ChartExportUtils {
 
-    public static JFreeChart merge(JFreeChart... charts) {
+    private static final String SVG_CHARSET = "UTF-8";
+    private static final boolean USE_CSS_IN_SVG_EXPORT = true;
+    private static final DOMImplementation DOM = GenericDOMImplementation.getDOMImplementation();
 
-        final JFreeChart merge;
-        if (charts == null || charts.length == 0) {
-            merge = null;
-        } else if (charts.length == 1) {
-            merge = charts[0];
-        } else {
-            merge = null;
+    public static void saveAsSVG(JFreeChart chart, int width, int height, File destination) throws IOException {
 
-            for (JFreeChart chart : charts) {
-                final Plot plot = chart.getPlot();
-                if (plot instanceof XYPlot) {
-                    final XYPlot xy = (XYPlot) plot;
-
-                    final XYDataset dataset = xy.getDataset();
-
-                }
-            }
+        final Document document = DOM.createDocument(null, "svg", null);
+        final SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+        Writer out = null;
+        try {
+            out = new OutputStreamWriter(new FileOutputStream(destination), SVG_CHARSET);
+            chart.draw(svgGenerator, new Rectangle(width, height));
+            svgGenerator.stream(out, USE_CSS_IN_SVG_EXPORT);
+            out.flush();
+        } finally {
+            IOUtils.closeQuietly(out);
         }
+    }
 
-        return merge;
+    public static void saveAsSerializedObject(JFreeChart chart, File destination) throws IOException {
+
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(new FileOutputStream(destination));
+            out.writeObject(chart);
+            out.flush();
+        } finally {
+            IOUtils.closeQuietly(out);
+        }
     }
 }
